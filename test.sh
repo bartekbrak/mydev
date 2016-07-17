2efbebb11d59cd9a479afbc8bb6352608d651659
Bartek Brak <bartek.rychlicki@gmail.com>
2016-07-17 14:18:05 +0200

    Make vagrant download and install conditional in test runner.
    Add virtualbox.

--- test.sh
+++ test.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-if [ ! -e vagrant_1.8.4_x86_64.deb ]
+if ! command -v vagrant >/dev/null 2>&1
 then
-    wget http://releases.hashicorp.com/vagrant/1.8.4/vagrant_1.8.4_x86_64.deb
+    if [ ! -e vagrant_1.8.4_x86_64.deb ]
+    then
+       wget http://releases.hashicorp.com/vagrant/1.8.4/vagrant_1.8.4_x86_64.deb
+    fi
+    sudo dpkg -i vagrant_1.8.4_x86_64.deb
 fi
-sudo dpkg -i vagrant_1.8.4_x86_64.deb
+sudo apt-get install -y virtualbox
 vagrant up
